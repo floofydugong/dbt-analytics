e724294baa828038ac2c4f05b312932d7daf94f6
John Huynh <inbox.johnhuynh@gmail.com>
2025-07-22 22:26:49 +0000

    refactored dim_customers to add two staging models. Set project configs to materialize all marts as table

--- models/marts/marketing/dim_customers.sql
+++ models/marts/marketing/dim_customers.sql
@@ -1,23 +1,14 @@
 with customers as (
 
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from raw.jaffle_shop.customers
+    select *
+    from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
 
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from raw.jaffle_shop.orders
+    select *
+    from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
@@ -30,7 +21,7 @@ customer_orders as (
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
 
-    from orders
+    from {{ ref('stg_jaffle_shop__orders') }}
 
     group by 1
 
@@ -46,7 +37,7 @@ final as (
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
-    from customers
+    from {{ ref('stg_jaffle_shop__customers') }} AS customers
 
     left join customer_orders using (customer_id)
 
